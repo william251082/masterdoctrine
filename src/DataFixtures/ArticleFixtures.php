8adc649a87207148e0e3dba009a0f420941a3e2e
william <william.del.rosario@iceshop.nl>
2019-09-27 17:26:35 +0200

    Many to many -- save

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,9 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Tag;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
+class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
@@ -57,8 +59,25 @@ EOF
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages))
             ;
+
+            /** @var Tag[] $tags*/
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            // see lazy loading in action
+//            dd($tags); die();
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+//                $tag->getName();
+//                dd($tag);
+            }
         });
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TagFixtures::class
+        ];
     }
 }
